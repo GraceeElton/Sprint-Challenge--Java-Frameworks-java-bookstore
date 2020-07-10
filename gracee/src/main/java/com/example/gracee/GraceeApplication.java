e71c88b9095863b75c156dd3eb710577b26aefa1
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-07-10 11:21:47 -0600

    Answered questions

--- gracee/src/main/java/com/example/gracee/GraceeApplication.java
+++ gracee/src/main/java/com/example/gracee/GraceeApplication.java
@@ -15,3 +15,15 @@ public class GraceeApplication
     }
 
 }
+
+// java questions
+
+//        Describe why Java is called an object oriented, strongly typed language.
+// - because Java is very structured and things have to be called the same thing
+//        Give examples of Java Collections explaining the use and differences between them.
+// - hashmap - is great but has no order
+//  - ArrayList - contains order but can not give away size easily.
+//        What is recursion and how does it differ from regular looping?
+// When the function calls itself  ????
+//        Explain the differences between Abstract Classes and Interfaces including scenarios of when to use them.
+// you got this explain animal kingdom!
